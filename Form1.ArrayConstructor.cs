c39db7266777e7f56387c1b30a5fcadc4f694c5e
jenna-n-cooper <jennacooper207@gmail.com>
2024-11-25 16:49:04 -0500

    changes

---
+++ Form1.ArrayConstructor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lines_and_Boxes
+{
+    public partial class Form1
+    {
+        private void InitializeCircleButtonArray()
+        {
+            int x = 5;
+            int y = 5;
+            int spacing = 5;
+            string buttonName = "circleButton";
+            CircleButton[,] CircleButtonArray = new CircleButton[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    buttonName = "CircleButton" + i.ToString() + j.ToString();
+                    CircleButtonArray[i, j] = new CircleButton();
+                    CircleButtonArray[i, j].BackColor = Color.Black;
+                    CircleButtonArray[i, j].Location = new Point(10 * i + spacing, 10 * j + spacing);
+                    circleButton1.Name = "circleButton" + i.ToString() + j.ToString();
+                    circleButton1.Size = new Size(40, 40);
+                    circleButton1.TabIndex = 1;
+                }
+            }
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(637, 450);
+            Controls.Add(circleButton1);
+            Name = "Form1";
+            Text = "Lines and Boxes";
+            ResumeLayout(false);
+        }
+    }
+}
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lines_and_Boxes
 {
-    partial class Form1
+    public partial class Form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -26,20 +26,41 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
+        
+      /* private void InitializeComponent()
         {
+            circleButton1 = new CircleButton();
             SuspendLayout();
+            // 
+            // circleButton1
+            // 
+            circleButton1.BackColor = Color.Black;
+            circleButton1.Location = new Point(12, 12);
+            circleButton1.Name = "circleButton1";
+            circleButton1.Size = new Size(40, 40);
+            circleButton1.TabIndex = 1;
+            circleButton1.Text = "circleButton1";
+            circleButton1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(637, 450);
+            Controls.Add(circleButton1);
             Name = "Form1";
             Text = "Lines and Boxes";
             ResumeLayout(false);
         }
-
+        */
         #endregion
+        private CircleButton[,] CircleButtonArray;
+        private CircleButton circleButton1;
+
+        public Form1()
+        {//change this so that we replace our new function with Initialize Component, keeping the same things
+           // InitializeComponent();//figure out how to initialize CircleButtonArray in the implementation
+            InitializeCircleButtonArray();
+        }
     }
 }
